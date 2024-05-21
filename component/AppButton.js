9175563e193cb0e6ac1abdf2a865f0656732c530
UshaBheri <127413962+UshaBheri@users.noreply.github.com>
2024-05-21 22:54:50 +0530

    AppButton.js

--- component/AppButton.js
+++ component/AppButton.js
@@ -1,17 +1,38 @@
-import React from 'react';
-import {TouchableOpacity,StyleSheet,View,Text} from 'react-native';
+import React from "react";
+import { TouchableOpacity, StyleSheet, View, Text } from "react-native";
 
-const AppButton = ({title,onPress,color='#fc565'}) =>{
-    return(
-        <TouchableOpacity
-            activeOpacity={0.7}
-            onPress={onPress}
-            style={Styles.cotainer}>
-            <View style = {[style.button,{backgroundColor:color}]}>
-                <Text style={styles.text}>{title}</Text>
-            </View>
-        </TouchableOpacity>
-    );
+const AppButton = ({ title, onPress, color = "#fc5c65" }) => {
+  return (
+    <TouchableOpacity
+      activeOpacity={0.7}
+      onPress={onPress}
+      style={styles.container}
+    >
+      <View style={[styles.button, { backgroundColor: color }]}>
+        <Text style={styles.text}>{title}</Text>
+      </View>
+    </TouchableOpacity>
+  );
 };
 
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: "center",
+    alignItems: "center",
+    paddingVertical: 10,
+    marginVertical: 10,
+  },
+  button: {
+    width: "60%",
+    borderRadius: 20,
+    height: 45,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 10,
+  },
+  text: {
+    fontSize: 15,
+    color: "white",
+  },
+});
 export default AppButton;
